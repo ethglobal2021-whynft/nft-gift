bb16877a4d52a6ba27339910444c1f80835488ca
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2021-10-06 00:27:38 +0300

    change models

--- src/web/models.py
+++ src/web/models.py
@@ -9,23 +9,17 @@ class Client(models.Model):
     created = models.DateTimeField(default=timezone.now)
 
 
-class EthereumWallet(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='ethereum_wallets')
-    address = models.CharField(max_length=42)
-    private_key = models.CharField(max_length=256, null=True, blank=True)  # todo: rehardcode should be deprecated, ofc
-
-
 class Gift(models.Model):
     sender = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='gifts_sent')
     receiver = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='gifts_received')
 
-    obtaining_url = models.URLField("Url that match receiver with a gift", unique=True)  # todo: create generator
+    obtaining_url = models.CharField("Url that match receiver with a gift", unique=True, max_length=256)  # todo: create generator
     text = models.TextField('Your wishes')
 
     rarible_url = models.URLField('Url to rarible post (aka token)')
     rarible_title = models.CharField(max_length=256)  # rarible has not its api for this
     rarible_description = models.TextField()  # rarible has not its api for this
-    rarible_creator_wallet = models.CharField(max_length=42)  # rarible has not its api for this
+    rarible_creator_address = models.CharField(max_length=42)  # rarible has not its api for this
 
     received = models.BooleanField(default=False)
     created = models.DateTimeField(default=timezone.now)
@@ -36,3 +30,8 @@ class Gift(models.Model):
 
         token_with_number = str(self.rarible_url).split('/')[-1]
         return f'https://img.rarible.com/staging/image/upload/t_big/staging-itemImages/{token_with_number}'
+
+    @cached_property
+    def url_to_contract_and_token(self) -> tuple:
+        numbers = str(self.rarible_url).split('/')[-1].split(':')
+        return numbers[0], numbers[1].split('?')[0]
