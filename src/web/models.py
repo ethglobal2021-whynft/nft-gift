cc1c65ee4d586691fcd68194455321851175c3f7
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2021-10-05 22:34:42 +0300

    change models behaviour

--- src/web/models.py
+++ src/web/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.functional import cached_property
 
 
 class Client(models.Model):
@@ -21,11 +22,17 @@ class Gift(models.Model):
     obtaining_url = models.URLField("Url that match receiver with a gift", unique=True)  # todo: create generator
     text = models.TextField('What do you want to wish')
 
-    rarible_image_url = models.URLField()  # rarible has not its api for this
+    rarible_url = models.URLField('Url to rarible post (aka token)')
     rarible_title = models.CharField(max_length=256)  # rarible has not its api for this
     rarible_description = models.TextField()  # rarible has not its api for this
-    rarible_author = models.CharField(max_length=42)  # rarible has not its api for this
+    rarible_creator_wallet = models.CharField(max_length=42)  # rarible has not its api for this
 
-    rarible_transfered_url = models.URLField(null=True, blank=True)
     received = models.BooleanField(default=False)
     created = models.DateTimeField(default=timezone.now)
+
+    @cached_property
+    def rarible_image_url(self):
+        """todo: hardcoded, better to convert token to pinata sources (no clue how)."""
+
+        token_with_number = str(self.rarible_url).split('/')[-1]
+        return f'https://img.rarible.com/staging/image/upload/t_big/staging-itemImages/{token_with_number}'
