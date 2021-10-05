bbef4071e40aaadd9fcbb36c31b10b29e8f7e4aa
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2021-10-06 00:40:58 +0300

    add var for testing

--- src/utils/rarible.py
+++ src/utils/rarible.py
@@ -4,7 +4,8 @@ import os
 
 
 logger = logging.getLogger(__name__)
-RARIBLE_API = os.getenv('RARIBLE_API', 'http://rarible_sdk:8080')
+RARIBLE_SDK_API = os.getenv('RARIBLE_SDK_API', 'http://rarible_sdk:8080')
+RARIBLE_SDK_OFF = int(os.getenv('RARIBLE_SDK_OFF', '0'))
 
 
 class RaribleSdkException(Exception):
@@ -12,13 +13,17 @@ class RaribleSdkException(Exception):
 
 
 def transfer_through_rarible_sdk(receiver: str, contract: str, token: str, sender_private_key: str) -> str:
+    if RARIBLE_SDK_OFF:
+        logger.info('Rarible SDK silenced =(...')
+        return 'foohash'
+
     data = {
         "user_receiver": receiver,
         "contract": contract,
         "token": token,
         "sender_private_ext": sender_private_key,
     }
-    r = requests.post(RARIBLE_API, json=data)
+    r = requests.post(RARIBLE_SDK_API, json=data)
     logger.info(f'requested rarible sdk with {data}, got response: {r.content}')
 
     j = r.json()
